f69911c3cb219b4be9980000478b22d0f6f104ea
Cesar Wong <cewong@redhat.com>
2017-09-26 15:45:26 -0400

    Fix location of logging configmap

--- vars/ensureLoggingComponents.groovy
+++ vars/ensureLoggingComponents.groovy
@@ -4,7 +4,7 @@ def call(Object ctx) {
   ctx.openshift.withCluster() {
     def o = ctx.openshift
     if (!o.selector("configmap/logging-config").exists()) {
-      ctx.applyTemplate(ctx, "logging-configmap.yaml")
+      ctx.applyTemplate(ctx, "cluster/ci/origin/logging-configmap.yaml")
     }
     if (!o.selector("secrets/gce").exists()) {
       o.raw("create secret generic gce")
