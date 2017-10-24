2913c540b857eb83a2449f38a78330e6cd6401f5
Cesar Wong <cewong@redhat.com>
2017-10-24 16:49:00 -0400

    saveArtifacts fixes

--- vars/saveArtifacts.groovy
+++ vars/saveArtifacts.groovy
@@ -4,14 +4,17 @@ def call(Object ctx, String artifactDir, Object selector) {
   ctx.openshift.withCluster() {
     selector.withEach() {
       def name = it.name()
-      ctx.sh "oc get ${name} -o yaml > ${artifactDir}/${name}.yaml"
-      ctx.sh "oc describe ${name} > ${artifactDir}/${name}.description"
+      def objectName = name.tokenize("/")[1]
+      ctx.sh "oc get ${name} -o yaml > ${artifactDir}/${objectName}.yaml"
+      ctx.sh "oc describe ${name} > ${artifactDir}/${objectName}.description"
       if (name.startsWith("pod/")) {
         def pod = it.object()
         for (def i = 0; i < pod.spec.containers.size(); i++) {
           def containerName = pod.spec.containers[i].name
-          ctx.sh "oc logs ${name} -c ${containerName} > ${artifactDir}/${name}_${containerName}.log"
+          ctx.sh "oc logs ${name} -c ${containerName} > ${artifactDir}/${objectName}_${containerName}.log"
         }
+      } else if (name.startsWith("build/")) {
+        ctx.sh "oc logs ${name} > ${artifactDir}/${objectName}.log"
       }
     }
   }
