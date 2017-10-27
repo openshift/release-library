51f59a9fea82e8c8743d661e02d5d80385b07643
Cesar Wong <cewong@redhat.com>
2017-10-27 17:08:14 -0400

    Upload artifacts for build pods, do not fail the build if a command fails when saving artifacts

--- vars/saveArtifacts.groovy
+++ vars/saveArtifacts.groovy
@@ -1,4 +1,27 @@
 #!/usr/bin/groovy
+
+// runCommand will report if an error occurs but will not fail the build
+def runCommand(ctx, cmd) {
+  try {
+    ctx.sh(script: cmd)
+  } catch(e) {
+    ctx.echo "error running ${cmd}: ${e}"
+  }
+}
+
+def savePodContainerLogs(ctx, artifactDir, podSelector, podName, pod) {
+  if (pod.spec.initContainers != null) {
+    for (def i = 0; i < pod.spec.initContainers.size(); i++) {
+      def containerName = pod.spec.initContainers[i].name
+      runCommand(ctx, "oc logs ${podSelector} -c ${containerName} > ${artifactDir}/${podName}_${containerName}.log")
+    }
+  }
+  for (def i = 0; i < pod.spec.containers.size(); i++) {
+    def containerName = pod.spec.containers[i].name
+    runCommand(ctx, "oc logs ${podSelector} -c ${containerName} > ${artifactDir}/${podName}_${containerName}.log")
+  }
+}
+
 def call(Object ctx, String artifactDir, Object selector) {
   ctx.sh "mkdir -p ${artifactDir}"
   ctx.openshift.withCluster() {
@@ -6,22 +29,22 @@ def call(Object ctx, String artifactDir, Object selector) {
       def name = it.name()
       def objectName = name.tokenize("/")[1]
       def objectType = name.tokenize("/")[0]
-      ctx.sh "oc get ${name} -o yaml > ${artifactDir}/${objectName}.yaml"
-      ctx.sh "oc describe ${name} > ${artifactDir}/${objectName}.description"
+      runCommand(ctx, "oc get ${name} -o yaml > ${artifactDir}/${objectName}.yaml")
+      runCommand(ctx, "oc describe ${name} > ${artifactDir}/${objectName}.description")
       if (objectType == "pods") {
         def pod = it.object()
-        if (pod.spec.initContainers != null) {
-          for (def i = 0; i < pod.spec.initContainers.size(); i++) {
-            def containerName = pod.spec.initContainers[i].name
-            ctx.sh "oc logs ${name} -c ${containerName} > ${artifactDir}/${objectName}_${containerName}.log"
+        savePodContainerLogs(ctx, artifactDir, name, objectName, pod)
+      } else if (objectType == "build") {
+        runCommand(ctx, "oc logs ${name} > ${artifactDir}/${objectName}.log")
+        def podName = it.object().metadata.annotations['openshift.io/build.pod-name']
+        if (podName != null && podName != "") {
+          def buildPod = openshift.selector("pod/${podName}")
+          if (buildPod.exists()) {
+            runCommand(ctx, "oc get pod/${podName} -o yaml > ${artifactDir}/${podName}.yaml")
+            runCommand(ctx, "oc describe pod/${podName} > ${artifactDir}/${podName}.description")
+            savePodContainerLogs(ctx, artifactDir, "pod/${podName}", podName, buildPod.object())
           }
         }
-        for (def i = 0; i < pod.spec.containers.size(); i++) {
-          def containerName = pod.spec.containers[i].name
-          ctx.sh "oc logs ${name} -c ${containerName} > ${artifactDir}/${objectName}_${containerName}.log"
-        }
-      } else if (objectType == "build") {
-        ctx.sh "oc logs ${name} > ${artifactDir}/${objectName}.log"
       }
     }
   }
